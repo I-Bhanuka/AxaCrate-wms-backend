8e6834c07688a8b7f9c6326f0cd30a30e0801250
Sheshan4587 <sheshanthisal3@gmail.com>
2026-02-12 13:04:24 +0530

    Add modifications to the createInventoryItem method

--- Backend/wms-application/src/main/java/com/axacrate/wms/service/InventoryService.java
+++ Backend/wms-application/src/main/java/com/axacrate/wms/service/InventoryService.java
@@ -1,17 +1,30 @@
 package com.axacrate.wms.service;
 
 import com.axacrate.wms.dto.InventoryItemRequestDTO;
+import com.axacrate.wms.dto.InventoryItemResponseDTO;
 import com.axacrate.wms.entity.InventoryItem;
 import com.axacrate.wms.entity.RfidTag;
 import com.axacrate.wms.entity.Zone;
+import com.axacrate.wms.exception.BusinessRuleViolationException;
+import com.axacrate.wms.exception.ResourceNotFoundException;
 import com.axacrate.wms.repository.InventoryItemRepository;
 import com.axacrate.wms.repository.RfidTagRepository;
 import com.axacrate.wms.repository.ZoneRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.domain.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import org.springframework.stereotype.Service;
 import lombok.extern.slf4j.Slf4j;
+
+import java.rmi.server.UID;
+import java.util.List;
+import java.util.UUID;
 
 @Slf4j
 @Service
@@ -28,30 +41,38 @@ public class InventoryService {
         this.zoneRepository = zoneRepository;
     }
 
-
+    @Transactional
     public void createInventoryItem(InventoryItemRequestDTO requestDTO) {
 
 
         // Find RFID Tag entity by UID
         RfidTag rfidTag = rfidTagRepository.findByUid(requestDTO.getRfidTag()).
-                orElseThrow(() -> new IllegalArgumentException("RFID Tag not found"));
+                orElseThrow(() -> new ResourceNotFoundException("RFID Tag not found"));
 
-        log.info("RFID Tag found: " + rfidTag.getUid());
+        // Check if RFID Tag is already assigned to an inventory item
+        if(rfidTag.getInventoryItem() != null) {
+            throw new BusinessRuleViolationException("RFID Tag already assigned");
+        }
+
+        log.info("RFID Tag found: {}", rfidTag.getUid());
 
         // Find Writer Zone
-        Zone zoneId = zoneRepository.findByZoneType(Zone.ZoneType.WRITER_ZONE).
+        Zone writerZone  = zoneRepository.findByZoneType(Zone.ZoneType.WRITER_ZONE).
                 orElseThrow(() -> new IllegalArgumentException("Writer zone not found"));
 
-        log.info("Zone found: " + zoneId);
+        log.info("Zone found: {}", writerZone );
 
         // Create Inventory Item entity
         InventoryItem entity = InventoryItem.builder()
                 .sku(requestDTO.getSku())
                 .name(requestDTO.getName())
                 .quantity(requestDTO.getQuantity())
-                .currentZone(zoneId)
+                .currentZone(writerZone)
                 .rfidTag(rfidTag)
                 .build();
+
+        // save to rfid tag the inventory item assignment
+        rfidTag.setInventoryItem(entity);
 
         // Save entity to database
         inventoryItemRepo.save(entity);
@@ -59,4 +80,96 @@ public class InventoryService {
         //TODO:
         // later send data to item id to esp32 for rfid tag assignment
     }
+
+    @Transactional(readOnly = true)
+    public Page<InventoryItemResponseDTO> getAllItems(
+            int page,
+            int size,
+            String sortString,
+            UUID zoneId,
+            Integer minQuantity,
+            Integer maxQuantity,
+            String status
+    ) {
+        log.info("Searching for inventory items: page={}, size={}, sort={}", page, size, sortString);
+
+        // Parse sort parameter
+        Sort sort = parseSortParameter(sortString);
+
+        // Create Pageable object
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        // Build dynamic query based on filters
+        // This allows combining multiple filters
+
+        Specification<InventoryItem> spec = Specification.where((Specification<InventoryItem>) null);
+
+        // Filter by zoneId
+        if (zoneId != null) {
+            spec = spec.and((root, query, cb) ->
+                    cb.equal(root.get("currentZone").get("id"), zoneId));
+        }
+
+        // Filter by minQuantity
+        if (minQuantity != null) {
+            spec = spec.and((root, query, cb) ->
+                    cb.greaterThanOrEqualTo(root.get("quantity"), minQuantity));
+        }
+
+        // Filter by maxQuantity
+        if (maxQuantity != null) {
+            spec = spec.and((root, query, cb) ->
+                    cb.lessThanOrEqualTo(root.get("quantity"), maxQuantity));
+        }
+
+        // Filter by status (assigned/unassigned/damaged/out of stock)
+        if (status != null) {
+            spec = spec.and((root, query, cb) ->
+                    cb.equal(root.get("status"), status));
+
+        }
+
+        // Execute query with specifications and pagination+
+        Page<InventoryItem> items = inventoryItemRepo.findAll(spec, pageable);
+
+        log.info("Retrieved {} items out of {} total",
+                items.getTotalElements(), items.getTotalElements());
+
+        // Map entities to DTOs
+        return items.map(item ->
+                InventoryItemResponseDTO.builder()
+                        .sku(item.getSku())
+                        .name(item.getName())
+                        .quantity(item.getQuantity())
+                        .build());
+    }
+
+    // Helper methods
+
+    /**
+     * Parse sort parameter from string to Sort object
+     * Format: "createdAt, desc"
+     */
+    private Sort parseSortParameter(String sortString) {
+      String[] split = sortString.split(",");
+
+      // below lines to avoid index out of bound error
+      String fields = split.length > 0 ? split[0] : "createdAt";
+      String direction = split.length > 1 ? split[1] : "desc";
+
+      // validate field name to avoid sql injection
+      List<String> validFields = List.of("sku", "name", "quantity", "createdAt", "updatedAt");
+
+      if (!validFields.contains(fields)) {
+          log.warn("Invalid sort parameter: {}", fields);
+          fields = "createdAt"; // default field
+      }
+
+      // Parse direction
+      Sort.Direction dir = direction.equalsIgnoreCase("asc") ? Sort.Direction.ASC : Sort.Direction.DESC;
+
+        return Sort.by(dir, fields);
+
+    }
+
 }
