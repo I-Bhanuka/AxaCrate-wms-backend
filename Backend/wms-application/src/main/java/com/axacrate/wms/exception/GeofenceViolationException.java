18f49adbdf0dcc7f29ca15c965908b9469376d54
Daniru130 <daniru130@gmail.com>
2026-03-03 12:22:31 +0530

    Update a geofenceViolationException to add serialversionId

--- Backend/wms-application/src/main/java/com/axacrate/wms/exception/GeofenceViolationException.java
+++ Backend/wms-application/src/main/java/com/axacrate/wms/exception/GeofenceViolationException.java
@@ -1,26 +1,25 @@
 package com.axacrate.wms.exception;
-
+import java.io.Serial;
+import java.util.UUID;
+import lombok.Getter;
 /**
  * Exception thrown when geofence rules are violated
  */
+@Getter
 public class GeofenceViolationException extends RuntimeException {
 
+    @Serial
     private static final long serialVersionUID = 1L;
 
+    private final UUID tagId;
     private final String fromZone;
     private final String toZone;
 
-    public GeofenceViolationException(String message, String fromZone, String toZone) {
+    public GeofenceViolationException(String message,UUID tagId, String fromZone, String toZone) {
         super(message);
+        this.tagId = tagId;
         this.fromZone = fromZone;
         this.toZone = toZone;
     }
 
-    public String getFromZone() {
-        return fromZone;
-    }
-
-    public String getToZone() {
-        return toZone;
-    }
 }
